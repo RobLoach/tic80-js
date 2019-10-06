83c912650158a9d570bf3ec97116875a20849eac
mhykes <mhykes@users.noreply.github.com>
2019-10-06 12:16:17 -0400

    Fixing TIC-80 PRO banks support.  Adding tests.

--- tests/index.js
+++ tests/index.js
@@ -2,11 +2,28 @@ const assert = require('assert');
 const TIC80JS = require('..')
 const fs = require('fs')
 
+
 describe('simple', function() {
 	it('builds the correct output', async function() {
 		const tic = new TIC80JS({input:'tests/simple'})
 		const output = await tic.build()
 		const code = fs.readFileSync('dist/cart.js', 'utf8')
 		assert(code.includes('function TIC(){'))
+	});
+});
+
+describe('banks', function() {
+	it('builds the correct output with banks', async function() {
+		const tic = new TIC80JS({input:'tests/banks', target: 'tic80pro'})
+		const output = await tic.build()
+		const code = fs.readFileSync('dist/cart.js', 'utf8')
+		let tag = `// <PALETTE>`
+		console.log(`Checking for ${tag}`)
+		assert(code.includes(tag))
+		for(let i=1; i<=7; i++) {
+			tag = `// <PALETTE${i}>`
+			console.log(`Checking for ${tag}`)
+			assert(code.includes(tag))
+		}
 	});
 });
